e8ac0678c169671fb4d72c50e7e2f51a1aa72838
driedm <gltan9265@gmail.com>
2025-11-09 01:18:42 -0500

    building array

---
+++ static/js/building.js
@@ -0,0 +1,118 @@
+export const buildingData = [
+    {
+        name: "berk",
+        lat: 772,
+        long: 1859,
+        
+    },
+    {
+        name: "bromery",
+        lat: 1335,
+        long: 1122,
+        
+    },
+    {
+        name: "campuscenter",
+        lat: 1753,
+        long: 1277,
+        
+    },
+    {
+        name: "chc",
+        lat: 1399,
+        long: 1952,
+    
+    },
+    {
+        name: "dubois",
+        lat: 1566,
+        long: 1532,
+    
+    },
+    {
+        name: "frank",
+        lat: 1505,
+        long: 437,
+    
+    },
+    {
+        name: "goodell",
+        lat: 1458,
+        long: 1700,
+    
+    },
+    {
+        name: "hamp",
+        lat: 964,
+        long: 1951,
+    
+    },
+    {
+        name: "ilc",
+        lat: 1674,
+        long: 1032,
+    },
+    {
+        name: "isenberg",
+        lat: 1314,
+        long: 872,
+    },
+    {
+        name: "lgrc",
+        lat: 1972,
+        long: 1417,
+    },
+    {
+        name: "manning",
+        lat: 2105,
+        long: 2008,
+    },
+    {
+        name: "morrill",
+        lat: 1674,
+        long: 722,
+    },
+    {
+        name: "northeast",
+        lat: 1314,
+        long: 872,
+    },
+    {
+        name: "ohill",
+        lat: 1314,
+        long: 872,
+    },
+    {
+        name: "oldchapel",
+        lat: 1314,
+        long: 872,
+    },
+    {
+        name: "pond",
+        lat: 1562,
+        long: 1282,
+    },
+    {
+        name: "reccenter",
+        lat: 1469,
+        long: 2182,
+    },
+    {
+        name: "stadium",
+        lat: 600,
+        long: 2500,
+    },
+    {
+        name: "stu",
+        lat: 1681,
+        long: 1411,
+    },
+    {
+        name: "woo",
+        lat: 1913,
+        long: 941,
+    }
+
+];
+
+
